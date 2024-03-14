aa258d3f658cf34ab74fb56274e9509a5654d3e2
CoinFu Master Shifu <145641460+CoinFuMasterShifu@users.noreply.github.com>
2024-03-14 22:30:21 +0100

    v0.2.5: add logs for node mining

--- src/miner/logs.hpp
+++ src/miner/logs.hpp
@@ -2,5 +2,6 @@
 #include "spdlog/spdlog.h"
 #include <memory>
 
-extern std::shared_ptr<spdlog::logger> mining_log;
+extern std::shared_ptr<spdlog::logger> stratum_mining_log;
+extern std::shared_ptr<spdlog::logger> node_mining_log;
 void initialize_mining_log();
